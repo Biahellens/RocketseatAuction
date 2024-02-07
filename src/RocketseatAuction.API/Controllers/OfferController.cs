4169cfd6f90865b8d053fb3817b224c6fc7454fa
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-02-07 17:46:46 -0300

    feat: melhorando o controller de offer

--- src/RocketseatAuction.API/Controllers/OfferController.cs
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RocketseatAuction.API.Communication.Requests;
 using RocketseatAuction.API.Filters;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
 
 namespace RocketseatAuction.API.Controllers;
 
 [ServiceFilter(typeof(AuthenticationUserAttibute))]
-    public class OfferController : RocketseatAuctionBaseController
-    {
+public class OfferController : RocketseatAuctionBaseController
+{
     [HttpPost]
     [Route("{itemId}")]
-    public IActionResult CreatedOffer([FromRoute] int itemId, [FromBody] RequestCreateOfferJson request)
-        {
-            return Created();
-        }
+    public IActionResult CreateOffer(
+        [FromRoute] int itemId,
+        [FromBody] RequestCreateOfferJson request,
+        [FromServices] CreateOfferUseCase useCase)
+    {
+        var id = useCase.Execute(itemId, request);
+
+        return Created(string.Empty, id);
     }
+}
